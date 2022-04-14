6129937a28e14ba46328c67b9d02488bc50d6b82
Mstars_lab1_04 <mstars_lab1_04@Mstars-lab1-04s-iMac.local>
2022-04-14 13:10:17 +0800

    butets

---
+++ front-end/dice/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import FormHeseg from './components/Form';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <FormHeseg />
+    </div>
+  );
+}
+
+export default App;
